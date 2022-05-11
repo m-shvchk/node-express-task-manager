b643b6bf520fcd8758621abeb35fbaccc93fae3f
m-shvchk <thirdinthequeue@gmail.com>
2022-05-11 15:13:57 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+// route
+app.get('/hello', (req, res) =>{
+    res.send('Task Manager App')
+})
+
+const port = 3000;
+app.listen(port, console.log(`server listening on port ${port}...`));
