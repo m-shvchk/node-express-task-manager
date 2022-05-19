7edb6e2b0427ab97c79549b848109188b5965cb6
m-shvchk <thirdinthequeue@gmail.com>
2022-05-19 22:01:56 +0300

    added basic validation for TaskShema and error handling for createTask controller

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,36 +1,40 @@
-const Task = require('../models/Task')
-
+const Task = require("../models/Task");
 
 const getAllTasks = (req, res) => {
-    res.send('get all tasks')
-}
+  res.send("get all tasks");
+};
+
 const createTask = async (req, res) => {
-    const task = await Task.create(req.body)
-    res.status(201).json({task})   
-}
-// With <model name>.create we create a new document in the DB. 
-// We pass req.body to it since there we exect to have { "name": "testing", "completed": true } 
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
+};
+// With <model name>.create we create a new document in the DB.
+// We pass req.body to it since there we exect to have { "name": "testing", "completed": true }
 // Finally we send the created in the DB document as the response:
 // { "task": { "_id": "627ffd9b39ff6348d84d8f50", "name": "testing", "completed": true, "__v": 0 } }
- 
 
 const getTask = (req, res) => {
-    res.json({id: req.params.id}) 
-    // for testing purposes: we make a get request for a specific item with "/api/v1/tasks/:id"
-    // where 'id' for testing purposes is any string   
-}
+  res.json({ id: req.params.id });
+  // for testing purposes: we make a get request for a specific item with "/api/v1/tasks/:id"
+  // where 'id' for testing purposes is any string
+};
+
 const updateTask = (req, res) => {
-    res.send('update task')   
-}
+  res.send("update task");
+};
+
 const deleteTask = (req, res) => {
-    res.send('delete task')   
-}
+  res.send("delete task");
+};
 
 module.exports = {
-    getAllTasks,
-    createTask,
-    getTask,
-    updateTask,
-    deleteTask,
-}
-
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+};
--- models/Task.js
+++ models/Task.js
@@ -1,10 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const TaskSchema = new mongoose.Schema({
-    name: String, completed: Boolean
-})
-// Each schema in Mongoose maps to a MongoDB collection and defines the shape of the documents within that collection with specified data types. 
+    name: {
+        type: String,
+        required: [true, "must provide name"], // the property must be set
+        trim: true, // trim whitespaces
+        maxlength: [20, "name cannot be more than 20 characters"]
+      },
+    completed: {
+        type:Boolean,
+        default: false,
+      },
+  });
+// Each schema in Mongoose maps to a MongoDB collection and defines the shape of the documents within that collection with specified data types.
 
-module.exports = mongoose.model("Task", TaskSchema)
+module.exports = mongoose.model("Task", TaskSchema);
 
 // An instance of a model is called a document. Models are responsible for creating and reading documents from the underlying MongoDB database. The first argument is the singular name of the collection your model is for. Mongoose automatically looks for the plural, lowercased version of your model name. Thus the model "Task" is for "tasks" collection
